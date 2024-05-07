d38fa22b58f17bc17e39999e32ad633b0409aae3
Harrison Williams <harrisontwilliams@gmail.com>
2024-05-06 20:17:01 -0700

    no OAuth, script auth working crawler v.1

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from RedditAuth import authenticate_reddit, fetch_posts
+from RedditAuth import authenticate_reddit
+from Fetch_Process_Data import fetch_posts, process_posts
+from DataStorer import DataStorage
 
 def main():
+    postLimit = 50
     reddit = authenticate_reddit()
-    fetch_posts(reddit)
+    subreddits = ['worldnews','science','space','sports','food','gadgets']
+    dataStore = DataStorage()
 
+    for subreddit in subreddits:
+        print("\nCrawling for post")
+        posts= fetch_posts(reddit,subreddit,postLimit)
+        processedPosts= process_posts(posts)
+        dataStore.store_data(processedPosts,subreddit)
+        print("Fetched,Processed & Stored Post")
 
 if __name__ == "__main__":
     main()
